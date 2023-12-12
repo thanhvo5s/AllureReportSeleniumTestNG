42a3a016f777f872d5cf8361b647675f3b44ab34
Thanh Vo <thanh.vo.82.vn@gmail.com>
2023-12-12 09:51:52 +0700

    Validate chromedriver file exist for CI
    Validate chromedriver file exist for CI

--- src/test/java/vss/welcome/WelcomeSelenium/BaseTest.java
+++ src/test/java/vss/welcome/WelcomeSelenium/BaseTest.java
@@ -1,12 +1,18 @@
 package vss.welcome.WelcomeSelenium;
 
 import org.openqa.selenium.WebDriver;
+import java.io.File;
 
 public class BaseTest {
 	WebDriver browser;
 	public BaseTest() {
-		System.out.println("driver path = " + System.getProperty("user.dir") + "\\chromedriver");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver");
+		System.out.println("driver path = " + System.getProperty("user.dir") + "//chromedriver");
+		File f = new File(System.getProperty("user.dir") + "//chromedriver");
+		if(f.exists() && !f.isDirectory()) { 
+			System.out.println("File Exists");
+		}
+		else System.out.println("File NOT Exists");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//chromedriver");
 	}
 	
 	public WebDriver GetDriver() {
